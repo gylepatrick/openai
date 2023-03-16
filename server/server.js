3dca96973f7fef99dfbe86b0b1c024b0a6b01063
gylepatrick <lukinhaygylepatrick@gmail.com>
2023-03-16 20:07:34 +0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// instance for openai
+const openai = new OpenAIApi(configuration);
+
+// init express
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello from Gyle!",
+  });
+});
+
+// delay function to add a delay between API requests
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    // add a delay of 1 second before sending the response
+    await delay(1000);
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+
+app.listen(5000, () => console.log("running"));
